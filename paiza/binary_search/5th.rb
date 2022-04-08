a6a25a1b5159fcfb5942e557e26cf57da43f9b4f
tomiswinner <mouseofsack911@gmail.com>
2022-04-08 14:55:44 +0000

    [ad] binary"

---
+++ paiza/binary_search/5th.rb
@@ -0,0 +1,12 @@
+n, k = gets.chomp.split.map!{|n| n.to_i}
+
+arr_pipe_len = gets.chomp.split.map!{|n| n.to_i}
+
+arr_pipe_len.sort!
+
+left,right
+
+mid = (left + right) / 2
+while 
+  mid = (left + right) / 2
+  for
