d9ebf729abd16e70f01dcb82c7c7e2313ea921c2
tomiswinner <mouseofsack911@gmail.com>
2022-04-13 15:09:06 +0000

    [add] binary

--- paiza/binary_search/6th.rb
+++ paiza/binary_search/6th.rb
@@ -7,9 +7,9 @@ $stdin = StringIO.new("5 3
 
 n_treasures, k = gets.chomp.split.map!{|n| n.to_i}
 
-arr_treasure = gets.chomp.split.map!{|n| n.to_i}
-
 arr_weight = gets.chomp.split.map!{|n| n.to_i}
+
+arr_treasure = gets.chomp.split.map!{|n| n.to_i}
 
 
 ok = 0.to_f
@@ -19,11 +19,14 @@ ng = 5001.to_f
   mid = (ng + ok) / 2
   arr_val = []
   arr_treasure.each_with_index do |elem, i|
+    # treasure / weight >= mid を変換 >> treasure - mid * weight >= 0
     arr_val << elem.to_f - arr_weight[i].to_f * mid
   end
   arr_val.sort!.reverse!
-  print "#{arr_val}\n"
-  if arr_val[0..k].sum >= 0
+  # print "#{arr_val}\n"
+  # print "total:#{arr_val[0..k].sum}\n"
+  # puts "#ok:#{ok}, ng:#{ng}"
+  if arr_val[0..k-1].sum >= 0
     ok = mid
   else
     ng = mid
@@ -39,3 +42,22 @@ puts ok
 # k個の treasure と　weight の平均価値（Σtreasure / Σweight）　が >= x であればよい
 # なぜ >= 0 の形にするかというと、割り算をすると数値が丸められる？（日本語正しいかは怪しいけど）から。
 # 極力掛け算の形などにしていく。
+
+# なんで平均値をソートして貪欲にとったらいけないのか >> これは % の計算だから。
+# 分数は分母を揃えないと計算できません。
+
+# なので実際に財宝を k 個とってきたと仮定し、
+# とってきた平均の値を計算してソートすれば、最大の方から貪欲に考えてよい
+
+# https://scrapbox.io/hkurokawa-cp/ABC_034_D_-_%E9%A3%9F%E5%A1%A9%E6%B0%B4
+# 上記リンクが非常にわかりやすい
+
+# やることは、条件（x%以上の平均値になる)をつけたうえで、条件以上の宝の価値をソートして貪欲する。
+
+# w=水の量、p=パーセント
+# 単純なパーセントでは測れないなぜなら、母数が大きいものほど、与える影響も多いから。
+# じゃあ食塩の量の大きさでみたらいいやってなるけど、その場合、食塩の量の大きさでも、水の量によって違うのでマイナスがありうる。(=食塩の量によって、パーセントに与える影響はプラスにもマイナスにもなりうる)
+# じゃあ、一定以上のパーセントの食塩の量だけを見る(=パーセントが変化しないものは0となり、線引きによって食塩の量を単純大きいか大きくないかで比較できるようになる、マイナスを排除してる)
+# 14:05
+# 単純に比較できないものは、
+# 条件を加えて比較する
---
+++ paiza/binary_search/7th.rb
@@ -0,0 +1,47 @@
+require "stringio"
+
+$stdin = StringIO.new("23 5 7
+2 6 9 10 12 15 19")
+
+# L cm n 人でわける、k 個の切れ目がもともとある
+L, n, k = gets.chomp.split.map!{|n| n.to_i}
+
+# i 番目の切れ目が左から a_i cm のとこにある
+
+arr_break = gets.chomp.split.map!{|n| n.to_i}
+arr_break << L
+arr_break.unshift(0)
+
+# 切れ目を n-1 個　選択、なるべく長く
+
+ok = 0
+ng = L
+
+while ng - ok > 1
+  mid = (ok + ng) / 2
+  puts "mid:#{mid}"
+  puts "ok:#{ok}"
+  puts "ng:#{ng}"
+  n - 1
+  tmp_len = 0
+  cnt_divided = 0
+  (arr_break.length-1).times do |i|
+    tmp_len += arr_break[i+1] - arr_break[i]
+    if tmp_len >= mid
+      cnt_divided += 1
+      tmp_len = 0
+    end
+  end
+  puts "cnt:#{cnt_divided}"
+  if k <= cnt_divided
+    ok = mid
+  else
+    ng = mid
+  end
+end
+
+puts ok
+
+# 最も短い太巻きが x 以上となるよう n　分割する
+# ならば y <= x の y は最も短い太巻きが y 以上となるよう n 分轄できる。
+
