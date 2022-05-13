00ee81977edd3e8f2fe6b8b1fbbaaf11cb3d4804
tomiswinner <mouseofsack911@gmail.com>
2022-05-13 23:54:32 +0900

    [add] java d

---
+++ paiza/java/Drank/1man.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int [] nums = new int[2];	
+		for(int i = 0; i < 2; i++){
+		  String line = sc.nextLine();
+		  nums[i]  = Integer.parseInt(line);
+		}
+		if(nums[0] * nums[1] >= 10000){
+			System.out.println("Yes");
+		}else{
+			System.out.println("No");
+		}
+	
+	}
+}
