eedf742a52e95ee517bbf51a99428cc7a1fab7da
tomiswinner <mouseofsack911@gmail.com>
2022-06-07 14:52:09 +0000

    [mod] 45 leet jump game ii_

--- paiza/python/DP/Leetcode/45.jumpgame2.py
+++ paiza/python/DP/Leetcode/45.jumpgame2.py
@@ -1,9 +1,21 @@
 class Solution:
   def jump(self, nums: List[int]) -> int:
-    n_now = nums[0]
-    i_now = 0
-    cnt = 0
-    while i_now + n_now < len(nums)-1:
-      maximum = max(nums[i_now+1:i_now+n_now+1])
+    next_arrival_point = nums[0]
+    max_jump_endpoint = 0
+    ans = 0
+    i = 1
+    while 
+      if i >= max_jump_endpoint:
+        max_jump_endpoint = next_endpoint
+        ans += 1
+      next_endpoint = max(next_arrival_point, i + N[i])
+      i += 1
       
 
+
+
+
+# https://dev.to/seanpgallivan/solution-jump-game-ii-cn3
+# N[i]にいる場合、N[i] = 4 であれば、N[i+1],N[i+2],N[i+3],N[i+4] が１ジャンプで到達できる最長到達点となる。
+# このとき、N[i+1~4]の中で、そこからジャンプしたときに最も遠くまで進める場所を選べばいい。
+# ただし、N[i+4]まで精査しない限り、どこでジャンプするかわからない。
