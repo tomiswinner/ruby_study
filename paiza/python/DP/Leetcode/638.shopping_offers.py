b5092ccbf94d6a9cd052fd1bc1b38cb8636c6af9
tomiswinner <mouseofsack911@gmail.com>
2022-06-12 20:52:33 +0900

    [mod] 638

--- paiza/python/DP/Leetcode/638.shopping_offers.py
+++ paiza/python/DP/Leetcode/638.shopping_offers.py
@@ -1,23 +1,28 @@
-
-# 方針　1,全て個別に単価で買う場合を計算とする
-# 2, セットが１～３あるのであれば、セット１を使用した場合、セット２、、と、DFSを使用する。
+from typing import List
+# 1, special からセット商品を使用した場合、needs から deducted される必要がある。
+# 2, セット商品購入に必要なそれぞれの商品の数量が needs で利用可能な数量以下である場合にのみ、セット商品での購入が可能である。
 
 class Solution:
     def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
-        # 全て単価で買う
-        val = sum(needs[i] * price[i] for i in range(len(needs))]
-        tmp_purchased = [number for number in needs]
-        for setitems in special:
-            tmp_val = 0
-            # 購入した分現在の価格に足す
-            for j in range(len(setitems)):
-                tmp_val += setitems[j] * price[j]
-            # 購入した個数分引き算する
-            for k in range(len(purchased)):
-                tmp_purchased[k] -= setitems[k]
-            # まだ必要な品物が買い切れていない場合、つぎ足す。
-            tmp_val_and_left = tmp_val + sum([tmp_purchased[l] * price[l] for l in range(len(price))])
-            tmp_val = min(tmp_val_and_left, dfs(tmp_purchased) + 
+        def dfs(t_needs):
+            for setitems in special:
+                # 現在の残っているneedsを全て単価で買う
+                tmp_val = 0
+                for j in range(len(setitems)):
+                    tmp_val += t_needs[j]  * price[j]
+                # セット商品を一つ購入したこととし、購入した個数分引き算する
+                for k in range(len(purchased)):
+                    t_needs[k] -= setitems[k]
+                # 必要数量以上購入していなければ、OK
+                if min(t_needs) >= 0:
+                    tmp_val = min(tmp_val, setitems[-1] + dfs(t_needs))
+
+        return dfs(needs)
+
+sol = Solution()
+print(sol.shoppingOffers([2,5],[[3,0,5],[1,2,10]],[3,2]))
+    
+
 
 
             
