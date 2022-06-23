120fbc111abaefc6d4778d7d2b16bc26dcaf1f07
tomiswinner <mouseofsack911@gmail.com>
2022-06-23 16:44:28 +0900

    [add] leet 494

---
+++ paiza/python/DP/Leetcode/494.target_sum.py
@@ -0,0 +1,34 @@
+from typing import List
+
+# TLE なので再考必要
+class Solution:
+    cnt = 0
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        total = 0
+        index = 0
+        self.dfs(index, total, nums, target)
+        return self.cnt
+        
+    def dfs(self, index, total, nums, target):
+        # print(f'index:{index}')
+        # print(f'total:{total}')
+        if index == len(nums)-1:
+            if total+nums[index] == target:
+                self.cnt += 1
+            if total-nums[index] == target:
+                self.cnt += 1
+        else:
+            self.dfs(index+1, total+nums[index], nums, target)
+            self.dfs(index+1, total-nums[index], nums, target)
+
+sol = Solution()
+print(sol.findTargetSumWays([1,1,1,1,1], 3))
+sol1 = Solution()
+print(sol1.findTargetSumWays([1],1))
+sol2 = Solution()
+print(sol2.findTargetSumWays([1,0],1))
+
+            
+            
+            
+
