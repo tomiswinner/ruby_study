77f6d7ea95e92aaabe051b4e804dab6c9ed735b4
tomiswinner <mouseofsack911@gmail.com>
2022-06-28 02:39:55 +0000

    [mod] leet 877

--- paiza/python/DP/Leetcode/877.stone_game.py
+++ paiza/python/DP/Leetcode/877.stone_game.py
@@ -14,8 +14,26 @@ class Solution:
         j = i + d
         dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1])
         print(f'd:{d},i:{i}, j:{j}')
+        print(f'piles[i:j]{piles[i:j+1]}')
         print(dp)
 
 sol = Solution()
 sol.stoneGame([5,3,4,5])
     
+
+class Solution:
+  def stoneGame(self, piles: List[int]) -> bool:
+    dp = {}
+
+    def dfs(l, r):
+      if l > r:
+        return 0
+      if (l, r) in dp:
+        return dp[(l, r)]
+      
+      isEven = True if (r-1) % 2 else False
+      left = piles[l] if isEven else 0
+      right = piles[r] if isEven else 0
+      
+      dfs(l+1, r)
+      dfs(l, r-1)
