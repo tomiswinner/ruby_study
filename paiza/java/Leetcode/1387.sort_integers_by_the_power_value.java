5496ca133718f3f213917fc87e65d5fd0f4ec334
tomiswinner <mouseofsack911@gmail.com>
2022-07-13 05:17:51 +0000

    [add] 1387

---
+++ paiza/java/Leetcode/1387.sort_integers_by_the_power_value.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Main{
+  public static void main(String[] args){
+    Solution sl = new Solution();
+    System.out.println(sl.getKth(12,15,2));
+
+  }
+}
+
+class Solution{
+  public int getKth(int lo, int hi, int k){
+    HashMap<Integer, Integer> dp = new HashMap<>();
+    dp.put(1, 0);
+
+    for (int i=2; i<hi; i++){
+      int now_int = i;
+      int cnt = 0;
+      while (true){
+        if (now_int % 2 == 0){
+          now_int /= 2;
+        } else {
+          now_int = now_int * 3 + 1;
+        }
+        cnt += 1;
+        
+        if (dp.get(now_int) != null){
+          dp.put(i, cnt + dp.get(now_int));
+          System.out.printf("dp[i]:%d\n", i);
+          System.out.println(dp.get(i));
+          break;
+        }
+      }
+    }
+    int[] answers = new int[hi-lo+1];
+    for( int i=0; i<answers.length; i++){
+      answers[i] = dp.get(lo+i);
+    }
+    Arrays.sort(answers);
+    return answers[k-1];
+  }
+}
