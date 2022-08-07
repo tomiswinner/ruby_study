9fc97b62dd31a4bc528b405035c1438c767e2722
tomiswinner <mouseofsack911@gmail.com>
2022-08-07 14:20:06 +0000

    [non

--- Leetcode_bests100/Graph/133.clone_graph_again.java
+++ Leetcode_bests100/Graph/133.clone_graph_again.java
@@ -27,13 +27,19 @@ class Node{
 
 class Solution{
   public Node cloneGraph(Node node){
-    List<Integer> queue = new LinkedList<>();
+    List<Node> queue = new LinkedList<>();
+    queue.add(node);
+    Map<Node, Node> mp = new HashMap<>();
     while(true){
       Node now = queue.remove(q.size()-1);
       for (int i; i<now.neighbors.size(), i++){
-        queue.add(now.neighbors.get(i));
+        // 次に訪れるノードを記憶
+        if (!mp.containsKey(now.neighbors.get(i))){
+          queue.add(now.neighbors.get(i));
+        }
       }
-
+      Node cp_now = new Node(now.val)
+      // 循環するから終わらないじゃん。だから map とかにしてたんだ。考え直し！
 
     }
 
