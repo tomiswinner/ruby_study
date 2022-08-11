7ca0def0461b8fdad639c2bdc8e0044ce85c20b8
tomiswinner <mouseofsack911@gmail.com>
2022-08-11 17:59:41 +0900

    [add] understand 133

--- Leetcode_bests100/Graph/133.clone_graph_again.java
+++ Leetcode_bests100/Graph/133.clone_graph_again.java
@@ -7,7 +7,7 @@ class Main{
 class Node{
   public int val;
   public List<Node> neighbors;
-
+	
   public Node(){
     val = 0;
     neighbors = new ArrayList<Node>();
@@ -27,21 +27,31 @@ class Node{
 
 class Solution{
   public Node cloneGraph(Node node){
+		
+		if (node == null) return null;
+
+
     List<Node> queue = new LinkedList<>();
     queue.add(node);
     Map<Node, Node> mp = new HashMap<>();
-    while(true){
-      Node now = queue.remove(q.size()-1);
-      for (int i; i<now.neighbors.size(), i++){
-        // 次に訪れるノードを記憶
-        if (!mp.containsKey(now.neighbors.get(i))){
-          queue.add(now.neighbors.get(i));
-        }
-      }
-      Node cp_now = new Node(now.val)
-      // 循環するから終わらないじゃん。だから map とかにしてたんだ。考え直し！
+		mp.put(node, new Node(node.val, new ArrayList<>()));
 
+    while(true){
+      Node now = queue.remove(queue.size()-1);
+      for (int i=0; i<now.neighbors.size(); i++){
+        // 次に訪れるノードを記憶&&次に訪れるノードを生成して mapへ追加
+				Node n = now.neighbors.get(i);
+        if (!mp.containsKey(n)){
+					mp.put(n, new Node(n.val, new ArrayList<>()));
+          queue.add(n);
+        }
+				mp.get(now).neighbors.add(mp.get(n));
+      }
+			if (queue.size() == 0){
+				break;
+			}
     }
+		return mp.get(node);
 
   }
 }
